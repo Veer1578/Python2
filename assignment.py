02ee60de0ac0f89ab8475f8956493b7aec1de363
Veer1578 <sweetyatulshah@gmail.com>
2025-04-02 09:36:38 +0530

    assignment

---
+++ assignment.py
@@ -0,0 +1,10 @@
+Vansh = "15 June"
+Yadnesh = "23 Oct"
+Aashray = "8 May"
+Ansh = "9 Dec"
+Aarya = "21 Jan"
+print("Your birthday is on", Vansh)
+print("Your birthday is on", Yadnesh)
+print("Your birthday is on", Aashray)
+print("Your birthday is on", Ansh)
+print("Your birthday is on", Aarya)
